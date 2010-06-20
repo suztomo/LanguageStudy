efe62bd78e2e35207b83cbdc55726002a489ee18
suztomo <suztomo+github@gmail.com>
2010-06-20 13:50:10 +0200

    type annotation

--- Haskell/YAHT/3.8_Interactivity.hs
+++ Haskell/YAHT/3.8_Interactivity.hs
@@ -6,9 +6,46 @@ module Main
     where
 
 import IO
+import Random
+
 main = do
   hSetBuffering stdin LineBuffering
-  putStrLn "Please enter your name: "
-  name <- getLine
-  putStrLn("Hello, " ++ name ++ ", how are you?")
+  num <- randomRIO (1::Int, 100)
+  putStrLn "I'm thinking of a number between 1 and 100"
+  doGuessing num
+
+{- ???? -}
+{-
+doGuessing num =
+    do
+      if 1 > 0
+      then putStrLn "Hello"
+      else putStrLn "tako"
+-}
+signum :: (Num a, Ord a) => a -> a
+{-
+The statement before '=>' means `a' is an instance of Num:
+Int, Double, etc.
+signum :: Int -> Int
+ -}
+signum x =
+    if x < 0
+      then -1
+      else if x > 0
+             then 1
+             else 0
+
+doGuessing num = do
+  putStrLn "Enter your guess: "
+  guess <- getLine
+  let guessNum = read guess
+  if guessNum < num
+    then do putStrLn "Too low!"
+            doGuessing num
+    else if read guess > num
+           then do putStrLn "Too high!"
+                   doGuessing num
+           else do putStrLn "You Win!"
+
+
 
