88b32dfe081a8b0b1346bcf6a76bb16d861037fb
suztomo <suztomo+github@gmail.com>
2010-06-22 16:58:24 +0200

    Actor test code added

--- Haskell/YAHT/7.7_Datatypes.hs
+++ Haskell/YAHT/7.7_Datatypes.hs
@@ -20,6 +20,7 @@ getHOstData (Configuration {localhost = lh, remotehost = rh}) = (lh, rh)
 data Tree a
     = Leaf a
     | Branch (Tree a) (Tree a)
+      deriving (Show)
 
 -- :kind FuncInt
 -- TupleThree :: * -> * -> * -> *
@@ -53,4 +54,41 @@ data C = F { f1, f2 :: Int, f3 :: Bool}
 data C = F Int Int Bool
 -}
 
+
+
+--instance Show a => Show (Tree a)  where
+--    show = showTree
+
+testTree =
+    Branch
+      (Branch
+        (Leaf 'a')
+        (Branch
+          (Leaf 'b')
+          (Leaf 'c')))
+      (Branch
+        (Leaf 'd')
+        (Leaf 'e'))
+
+showTree                :: (Show a) => Tree a -> String
+showTree (Leaf x)       =  "Leaf " ++ (show x)
+showTree (Branch l r)   =  "<" ++ showTree l ++ "|" ++ showTree r ++ ">"
+
+data Tako a = Tako a
+
+{-
+instance Show (Tako a) where
+    show (Tako a) = "Tako(" ++ show a ++ ")"
+
+7.7_Datatypes.hs:80:31:
+    Could not deduce (Show a) from the context (Show (Tako a))
+      arising from a use of `show' at 7.7_Datatypes.hs:80:31-36
+    Possible fix:
+      add (Show a) to the context of the instance declaration
+
+That means 
+instance Show a => Show (Tako a) where
+    show (Tako a) = "Tako(" ++ show a ++ ")"
+
+-}
 
