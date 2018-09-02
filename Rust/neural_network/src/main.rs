b4a74fa68d4ac8cf2cd71685a721241661c28275
suztomo <suztomo+github@gmail.com>
2018-09-01 21:46:43 -0400

    Neural network works!

--- Rust/neural_network/src/main.rs
+++ Rust/neural_network/src/main.rs
@@ -7,6 +7,7 @@ use ndarray::prelude::*;
 extern crate utils;
 use utils::math::sigmoid;
 
+
 // Rust implementation of Neural Network learning:
 // https://towardsdatascience.com/how-to-build-your-own-neural-network-from-scratch-in-python-68998a08e4f6
 
@@ -16,15 +17,29 @@ use utils::math::sigmoid;
 // Handwritten digit recognizer
 // http://handwritten-digits-recognizer.herokuapp.com/
 
+// Youtube video https://www.youtube.com/watch?v=tIeHLnjs5U8
+// Backpropagation calculus | Deep learning, chapter 4
+
 // Let's say input is (a:f32, b:f32, c:f32). This is Array1<f32>
 #[derive(Debug)]
 struct NeuralNetwork{
     // <row count> x <column count>
     last_input: Array2<f32>, // Input 1 x 3
     weight1: Array2<f32>,   // 3 x 4
+    layer1_a: Array2<f32>,   // 1 x 4, before activation
     layer1: Array2<f32>,    // 1 x 4
     weight2: Array2<f32>,   // 4 x 1
+    last_output_a: f32,     // before activation
     last_output: f32, // scalar
+}
+
+fn tanh(x: f32) -> f32 {
+    x.tanh()
+}
+
+fn tanh_prime(x: f32) -> f32 {
+    let t = tanh(x);
+    1. - t*t
 }
 
 fn sigmoid_derivative(x: f32) -> f32 {
@@ -33,6 +48,21 @@ fn sigmoid_derivative(x: f32) -> f32 {
     t * (1.0 - t)
 }
 
+fn bad_sigmoid_derivative(x: f32) -> f32 {
+    // https://iamtrask.github.io/2015/07/12/basic-python-network/
+    x * (1.0 - x)
+}
+
+
+// This works with back_prop_prml
+const ACTIVATION:&Fn(f32) -> f32 = &sigmoid;
+const ACTIVATION_PRIME: &Fn(f32) -> f32 = &sigmoid_derivative;
+// This works with back_prop_
+//const ACTIVATION:&Fn(f32) -> f32 = &sigmoid;
+//const ACTIVATION_PRIME: &Fn(f32) -> f32 = &bad_sigmoid_derivative;
+//const ACTIVATION:&Fn(f32) -> f32 = &tanh;
+//const ACTIVATION_PRIME: &Fn(f32) -> f32 = &tanh_prime;
+
 impl NeuralNetwork {
     fn feed_forward(&mut self, input_array: ArrayView1<f32>) -> f32 {
         self.last_input = input_array.to_owned().insert_axis(Axis(0));
@@ -40,22 +70,79 @@ impl NeuralNetwork {
         //   'ndarray: inputs 3 × 2 and 3 × 1 are not compatible for matrix multiplication'
 
         // lastInput: 1 x 3, weight1: 3 x 4, and thus dot_product: 1 x 4
-        let dot_product:Array2<f32> = self.last_input.dot(&self.weight1);
+        self.layer1_a = self.last_input.dot(&self.weight1);
         // https://docs.rs/ndarray/0.11.2/ndarray/struct.ArrayBase.html#method.mapv
         // layer1: 1 x 4
-        self.layer1 = dot_product.mapv(sigmoid);
-
+        self.layer1 = self.layer1_a.mapv(ACTIVATION);
+ 
         // When the matrix size is different:
         //   'assertion failed: `(left == right)`
         //     left: `4`,
         //     right: `3`'
         // layer1: 1 x 4, weight2: 4 x 1, and thus output is 1 x 1 (Array2)
         let output_matrix:Array2<f32> = self.layer1.dot(&self.weight2);
-        let output_before_sigmoid:f32 = output_matrix[[0, 0]];
-        self.last_output = sigmoid(output_before_sigmoid);
+        // before activation
+        self.last_output_a = output_matrix[[0, 0]];
+        self.last_output = ACTIVATION(self.last_output_a);
         self.last_output
     }
 
+    fn back_prod_prml(&mut self, y: f32) {
+        // Input layer     hidden layer       output layer
+        //   3 -> (weights1) -> 4 -> (weights2) -> 1
+
+        // PRML p.244 "Error Backpropagation"
+        // 1. "Apply an input vector..." is already done at feedforward function
+
+        // 2. Evaluate δ (delta) for all hte output unit. In this case it's only one value
+        // Get Error δ (delta) at the output, the output layer only has one element so f32 is enough.
+        let delta_output:f32 = y - self.last_output;
+
+
+        // 3. Backpropagate deltas using next delta and weight
+        // In this case, hidden Layer has 4 nodes. So the error (delta) is 4-element matrix
+        //     self.layer1_a : 1 x 4 matrix
+        // hidden_delta_0 = h'(self.layer1_a[[0, 0]]) * (weights2[[0, 0]] * delta_output)
+        // hidden_delta_1 = h'(self.layer1_a[[0, 1]]) * (weights2[[1, 0]] * delta_output)
+        // hidden_delta_2 = h'(self.layer1_a[[0, 2]]) * (weights2[[2, 0]] * delta_output)
+        // hidden_delta_3 = h'(self.layer1_a[[0, 3]]) * (weights2[[3, 0]] * delta_output)
+
+        // self.weight2 is 4x1 matrix, so as weights2_by_delta_output
+        let weights2_by_delta_output: Array2<f32> = self.weight2.mapv(|i| i * delta_output);
+        // 4x1 matrix element-wise multiplied by 4x1 (transpose of 1x4) matrix
+        // delta_hidden: 4x1
+        let layer1_a_derived = self.layer1_a.t().mapv(ACTIVATION_PRIME);
+        let delta_hidden: Array2<f32> =  weights2_by_delta_output * layer1_a_derived;
+
+
+        // 4. Use the deltas to evaluate the derivatives
+        // Error derivative with respect to weight2 (4x1 matrix). self.layer1: 1 x 4
+        // dE / weight2[0][0] = delta_output * self.layer1[[0, 0]]
+        // dE / weight2[1][0] = delta_output * self.layer1[[0, 1]]
+        // dE / weight2[2][0] = delta_output * self.layer1[[0, 2]]
+        // dE / weight2[3][0] = delta_output * self.layer1[[0, 3]]
+
+        // As weights2: 4x1, we want d_weights2 as 4x1. So transpose self.layer1
+        let d_weights2: Array2<f32> = self.layer1.t().mapv(|i| i * delta_output);
+
+        // As weights1: 3x4, we want d_weights1 as 3x4. delta_hidden: 4x1, last_input: 1x3
+        // dE / weight1[0][0] = delta_hidden[[0, 0]] * self.last_input[[0, 0]]
+        // dE / weight1[0][1] = delta_hidden[[1, 0]] * self.last_input[[0, 0]]
+        // dE / weight1[0][2] = delta_hidden[[2, 0]] * self.last_input[[0, 0]]
+        // dE / weight1[0][3] = delta_hidden[[3, 0]] * self.last_input[[0, 0]]
+        // dE / weight1[1][0] = delta_hidden[[0, 0]] * self.last_input[[0, 1]]
+        // ...
+        // dE / weight1[2][3] = delta_hidden[[3, 0]] * self.last_input[[0, 2]]
+        
+        // Is this d_weights1 = delta_hidden.dot(&self.last_input) ?
+
+        // delta_hidden: 4x1 multiplied by last_input: 1x3 = 4 x 3
+        let delta_hidden_dot_input: Array2<f32> = delta_hidden.dot(&self.last_input);
+        let d_weights1: ArrayView2<f32> = delta_hidden_dot_input.t();
+
+        self.weight2 += &(d_weights2.mapv(|i| i ));
+        self.weight1 += &(d_weights1.mapv(|i| i ));
+    }
 
     fn back_prop(&mut self, y: f32) {
         // d_weights2 = np.dot(self.layer1.T, (2*(self.y - self.output) * sigmoid_derivative(self.output)))
@@ -65,16 +152,16 @@ impl NeuralNetwork {
         //                      *
         //                      sigmoid_derivative(self.layer1)))
         let y_diff = y - self.last_output;
-        let sigmoid_deriv_last_output:f32 = sigmoid_derivative(self.last_output);
+        let last_output_deriv:f32 = ACTIVATION_PRIME(self.last_output);
         // layer1.t: 4 x 1, and d_weight2: 4 x 1
-        let d_weights2: Array2<f32> = self.layer1.t().mapv(|i| i * (2.0 * y_diff * sigmoid_deriv_last_output));
+        let d_weights2: Array2<f32> = self.layer1.t().mapv(|i| i * (2.0 * y_diff * last_output_deriv));
 
         // weight2.t(): 1 x 4, so k: 1 x 4
-        let k:Array2<f32> = self.weight2.t().mapv(|i| i * 2.0 * y_diff * sigmoid_deriv_last_output);
+        let k:Array2<f32> = self.weight2.t().mapv(|i| i * 2.0 * y_diff * last_output_deriv);
         // layer1: 1 x 4, sigmoid_deriv_layer1: 1 x 4
-        let sigmoid_deriv_layer1: Array2<f32> = self.layer1.mapv(sigmoid_derivative);
+        let layer1_deriv: Array2<f32> = self.layer1.mapv(ACTIVATION_PRIME);
         // k: 1 x 4 and sigmoid_deriv_layer1: 1 x 4, element-wise multiplication. m: 1 x 4
-        let m: Array2<f32> = k * sigmoid_deriv_layer1;
+        let m: Array2<f32> = k * layer1_deriv;
 
         // "From" is only for Vec https://docs.rs/ndarray/*/ndarray/type.Array2.html
         //   let t_matrix: Array2<f32> = Array2::From(t);
@@ -116,7 +203,9 @@ fn main() {
         last_input: arr2(&[[]]),
         weight1 : init_weight1,
         weight2 : init_weight2,
+        last_output_a: 0.0,
         last_output: 0.0,
+        layer1_a: arr2(&[[]]), 
         layer1 : arr2(&[[]]),
     };
     //let input1 = arr1(&[0.23, 0.21, -0.5]);
@@ -129,18 +218,20 @@ fn main() {
         [1., 1., 1.]
         ]);
     // XOR: 0., 1., 1., 0.
-    let answers = vec![0., 1., 1., 0.];
+    let answers = vec![0., 0., 0., 1.];
+//    let answers = vec![1., 1., 0.];
     //println!("training data {:?} {:?}", training_data, ans);
     
     let t_shape = training_data.shape();
 
-    for j in 0..1500 {
+    for j in 0..10000 {
         for i in 0..t_shape[0] {
             let tdata:ArrayView1<f32> = training_data.slice(s![i, ..]);
             let tmp_ans = nn.feed_forward(tdata);
             let a = answers[i];
-            nn.back_prop(a);
-            println!("training {}! Diff: {}", j, tmp_ans - a);
+            //nn.back_prop(a);
+            nn.back_prod_prml(a);
+            //println!("training {}! Diff: {}", j, tmp_ans - a);
         }
     }
 
@@ -148,7 +239,7 @@ fn main() {
     for i in 0..t_shape[0] {
         let tdata:ArrayView1<f32> = training_data.slice(s![i, ..]);
         let output:f32 = nn.feed_forward(tdata);
-        println!("{:?} = {}", tdata, output);
+        println!("{:?} => actual {}, expected {}", tdata, output, answers[i]);
     }
 
 }
